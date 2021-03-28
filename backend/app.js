7b14437a449c4ab20219f20c22fb8a86d6dd638c
Yulia Nesterova <julianstrv@yandex.ru>
2021-03-28 13:20:29 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -19,15 +19,15 @@ const options = {
 
 const app = express();
 
-app.use('*', cors(options));
-
-app.use(function(req, res, next) {
+app.use('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'https://mesto.nesterova.students.nomoredomains.icu');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
   next();
 });
+
+app.use('*', cors(options));
 
 const { PORT = 3001 } = process.env;
 
