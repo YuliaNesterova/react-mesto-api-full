fd62987320a6c7d6cbfdb4e9eb3a7b316b7c714b
Yulia Nesterova <julianstrv@yandex.ru>
2021-03-31 22:27:58 +0300

    fix: fix header links

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -24,21 +24,26 @@ export default function Header(props) {
 
     return (
         <header className="header">
-            <div className="header__buttons">
+            <div className="header__main-info">
                 <img src={logo} alt="Место" className="header__logo"/>
-                <button className={`header__menu-button ${isInfoShown ? 'header__display_type_none' : `header__display`}`}
-                onClick={handleMenuShow}></button>
-                <button className={`header__close-button ${isInfoShown ? `header__display` : `header__display_type_none`}`}
-                onClick={handleMenuHide}></button>
+                <div className={`header__buttons ${props.loggedIn ? `header__display` : `header__display_type_none`}`}>
+                    <button className={`header__menu-button ${isInfoShown ? 'header__display_type_none' : `header__display`}`}
+                    onClick={handleMenuShow}></button>
+                    <button className={`header__close-button ${isInfoShown ? `header__display` : `header__display_type_none`}`}
+                    onClick={handleMenuHide}></button>
+                </div>
             </div>
+
             {props.loggedIn ? <div className="header__wrapper">
                 <p className={`header__email ${isInfoShown ? `header__display`: ''}`}>{props.userEmail}</p>
                 <Link to="/sign-in"
-                      className={`header__link header__link_type_signout ${isInfoShown ? `header__display`: ''}`}
+                      className={`header__signout-link ${isInfoShown ? `header__display`: ''}`}
                       onClick={handleSignOut}>Выйти</Link>
             </div> : props.isLoginActive ? <Link to="/sign-up" className="header__link"
                                                  onClick={props.handleLoginInactive}>Регистрация</Link> :
                     <Link to="/sign-in" className="header__link" onClick={props.handleLoginActive}>Войти</Link>}
+
+
         </header>
     );
 }
